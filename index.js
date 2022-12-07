e65ac618020e4ac3f9dbd225faecce125399ee5e
Nikhil D R <Nikhildr22@gmail.com>
2022-12-08 04:08:07 +0530

    changes

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const posts = [
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
         likes: 21
+    }
+    ,
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
     },
     {
         name: "Gustave Courbet",
@@ -16,15 +26,30 @@ const posts = [
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
         likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
     }
 ]
 
+const renderTarget = document.getElementById("container")
+
+let dataToRender = "";
+
+for(let i=0;i<posts.length;i++){
+    dataToRender += `
+    <div class="card">
+                    <div class="card-header">
+                    </div>
+                    <div class="card-body">
+
+                        <img src="${posts[i].post}" alt="" class="card-body-img" >
+                        <div class="card-body-buttons">
+                        </div>
+                        <div class="card-body-details">
+                            <p class="card-body-likesummary">${posts[i].likes} Likes </p>
+                            <p class="card-body-caption"><span id="card-body-username">${posts[i].username}</span> ${posts[i].comment} </p>
+                        </div>
+                    </div>
+                </div>
+    `
+}
+
+renderTarget.innerHTML = dataToRender
